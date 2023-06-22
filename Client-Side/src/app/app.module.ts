ce63953bf27059555db2b2e0c7ecc584f3de74b4
mourad_br <mouradbarkouch@gmail.com>
2023-06-22 17:14:15 +0100

    customers AND acounts

--- Client-Side/src/app/app.module.ts
+++ Client-Side/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AccountsComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
--- Client-Side/src/app/nav-bar/nav-bar.component.ts
+++ Client-Side/src/app/nav-bar/nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'navbar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
